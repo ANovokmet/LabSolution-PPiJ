a9926bb4964c5eb00170d65ed6b8b03210545847
Goran Rumin <goran1406@gmail.com>
2015-04-06 18:23:58 +0200

    Pocetni commit

---
+++ LabSolution/android/src/com/swag/solutions/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.swag.solutions.android;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.swag.solutions.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MainGame(), config);
+	}
+}
---
+++ LabSolution/desktop/src/com/swag/solutions/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.swag.solutions.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.swag.solutions.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height=600;
+        config.width=1024;
+		new LwjglApplication(new MainGame(), config);
+	}
+}
