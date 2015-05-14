ccb1b64fd78096beb98bb5b41a92a76c21a951a5
ANovokmet <ante.novokmet@fer.hr>
2015-05-15 00:04:20 +0200

    1/2

--- LabSolution/android/src/com/swag/solutions/android/AndroidLauncher.java
+++ LabSolution/android/src/com/swag/solutions/android/AndroidLauncher.java
@@ -1,6 +1,9 @@
 package com.swag.solutions.android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -8,6 +11,8 @@ import com.swag.solutions.LabGame;
 import com.swag.solutions.MainGame;
 
 public class AndroidLauncher extends AndroidApplication {
+    protected LabGame labGame;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,6 +20,25 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = true;
 
-		initialize(new LabGame(), config);
+        labGame = new LabGame();
+		initialize(labGame, config);
 	}
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        labGame.pause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        labGame.pause();
+        return true;
+    }
+
 }
